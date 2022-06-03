55b51b84be6d2d37d20fe619eec70dfee7e12b7d
The10thDoctor1 <106801823+The10thDoctor1@users.noreply.github.com>
2022-06-03 11:51:39 +0200

    Adds initial ball movement and restructures code

--- main.py
+++ main.py
@@ -3,26 +3,46 @@ PADDLE_Y = 4
 PADDLE_LENGTH = 3
 ball_x = 2
 ball_y = 2
+change_x = 0
+change_y = -1
+step_duration = 500 # game speed
+
 
 def button_pressed_a():
     global paddle_x
     if paddle_x > 0:
         paddle_x = paddle_x - 1
+input.on_button_pressed(Button.A, button_pressed_a)
+
 
 def button_pressed_b():
     global paddle_x
     if paddle_x < 5 - PADDLE_LENGTH:
         paddle_x = paddle_x + 1
+input.on_button_pressed(Button.B, button_pressed_b)
 
-def loop():
+
+def render():
     basic.clear_screen()
 
     led.plot(ball_x, ball_y)
     for i in range(3):
         led.plot(paddle_x + i, PADDLE_Y)
+          
+    basic.pause(step_duration)
 
-    basic.pause(200)
 
-input.on_button_pressed(Button.A, button_pressed_a)
-input.on_button_pressed(Button.B, button_pressed_b)
+def loop():
+    global ball_x, ball_y
+
+    # input processing happens asynchronously
+
+    # update
+    ball_x = ball_x + change_x
+    ball_y = ball_y + change_y
+
+    render() 
+
+
+render()
 basic.forever(loop)
--- main.ts
+++ main.ts
@@ -3,6 +3,10 @@ let PADDLE_Y = 4
 let PADDLE_LENGTH = 3
 let ball_x = 2
 let ball_y = 2
+let change_x = 0
+let change_y = -1
+let step_duration = 500
+//  game speed
 input.onButtonPressed(Button.A, function button_pressed_a() {
     
     if (paddle_x > 0) {
@@ -17,11 +21,21 @@ input.onButtonPressed(Button.B, function button_pressed_b() {
     }
     
 })
-basic.forever(function loop() {
+function render() {
     basic.clearScreen()
     led.plot(ball_x, ball_y)
     for (let i = 0; i < 3; i++) {
         led.plot(paddle_x + i, PADDLE_Y)
     }
-    basic.pause(200)
+    basic.pause(step_duration)
+}
+
+render()
+basic.forever(function loop() {
+    
+    //  input processing happens asynchronously
+    //  update
+    ball_x = ball_x + change_x
+    ball_y = ball_y + change_y
+    render()
 })
