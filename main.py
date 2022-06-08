e8578fcf8a3568d5d0056b235a8e730666e659ee
The10thDoctor1 <106801823+The10thDoctor1@users.noreply.github.com>
2022-06-08 09:19:36 +0200

    Adds bouncing ball for paddle length 3

--- main.py
+++ main.py
@@ -71,6 +71,31 @@ def loop():
                                         if ball_x == 4 and led.point(4, 4):
                                             change_y = -1
 
+    # bouncing ball by 3 long paddle
+    if ball_y == 3 and PADDLE_LENGTH == 3:
+        if ball_x == 0 and led.point(0, 4):
+            change_y = -1
+        
+        else:
+            if ball_y == 3 and PADDLE_LENGTH == 3:
+                if ball_x == 1 and led.point(1, 4):
+                    change_y = -1
+
+                else:
+                    if ball_y == 3 and PADDLE_LENGTH == 3:
+                        if ball_x == 2 and led.point(2, 4):
+                            change_y = -1
+
+                        else:
+                            if ball_y == 3 and PADDLE_LENGTH == 3:
+                                if ball_x == 3 and led.point(3, 4):
+                                    change_y = -1
+
+                                else:
+                                    if ball_y == 3 and PADDLE_LENGTH == 3:
+                                        if ball_x == 4 and led.point(4, 4):
+                                            change_y = -1
+
     render() 
 
 
